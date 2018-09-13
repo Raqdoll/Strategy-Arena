2b0182a5a5a6289cc8ef6b6320ee840882d05678
Raqdoll <ilarisalonen112@gmail.com>
2018-09-13 11:41:50 +0300

    Initial

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+
+    public GameObject tiles;
+    public Tile[] tileList;
+
+	// Use this for initialization
+	void Start () {
+        tileList = tiles.GetComponentsInChildren<Tile>();
+
+        //for(int i = 0; i < 24; i++)
+        //{
+        //    for (int j = 0; j < 24; j++)
+        //    {
+        //        int count = i * 24 + j; /*BaseBlock, ShootThroughBlock, BlockyBlock, StartA, StartB*/
+        //        switch (tileList[count].myType)
+        //        {
+        //            case :
+        //                break;
+        //        }
+        //    }
+        //}
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public int locX;
+    public int locZ;
+
+    public Material ShootThroughBlockMaterial;
+    public Material BlockyBlockMaterial;
+    private Material thisMaterial;
+
+    public bool isFree = true;
+    private bool ShootThrough;
+    private bool Targetable;
+    private bool WalkThrough;
+    public enum BlockType { BaseBlock, ShootThroughBlock, BlockyBlock, StartA, StartB};
+    public BlockType myType;
+
+	// Use this for initialization
+	void Start () {
+
+        locX = (int)transform.localPosition.x;
+        locZ = (int)transform.localPosition.z;
+        thisMaterial = GetComponent<Renderer>().material;
+
+        switch (myType)
+        {
+            case BlockType.BaseBlock:
+                ShootThrough = true;
+                Targetable = false;
+                WalkThrough = true;
+            break;
+
+            case BlockType.ShootThroughBlock:
+                ShootThrough = true;
+                Targetable = false;
+                WalkThrough = false;
+                //thisMaterial.color =  ShootThroughBlockMaterial.color;
+                GetComponent<Renderer>().gameObject.SetActive(false);
+            break;
+
+            case BlockType.BlockyBlock:
+                ShootThrough = false;
+                Targetable = false;
+                WalkThrough = false;
+                thisMaterial.color = BlockyBlockMaterial.color;
+                transform.localScale += new Vector3(0.1f, 0.55f, 0.1f);
+                transform.position += new Vector3(0, 0.3f, 0);
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
